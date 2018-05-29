cf28320654d2021ea429b3b08f49e99e467e7a34
louvkii <0Elfonfire@gmail.com>
2018-05-29 15:32:52 +0200

    eh

--- www/acs.js
+++ www/acs.js
@@ -1,20 +1,11 @@
 // Empty constructor
-function Acs() { }
+var ACS = {}
 
 // The function that passes work along to native shells
 // Message is a string, duration may be 'long' or 'short'
 
-Acs.connectReader = function (readerAddress, success, failure) {
+ACS.connectReader = function (readerAddress, success, failure) {
   cordova.exec(success, failure, "CardReader", "connectReader", [readerAddress]);
 };
 
-
-// Installation constructor that binds Acs to window
-Acs.install = function () {
-  if (!window.plugins) {
-    window.plugins = {};
-  }
-  window.plugins.acs = new Acs();
-  return window.plugins.acs;
-};
-cordova.addConstructor(Acs.install);
+window.ACS = ACS;
