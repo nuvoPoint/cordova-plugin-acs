078f53b0be2c65654e54f77fd77887566fec0929
louvkii <0Elfonfire@gmail.com>
2018-05-31 14:38:37 +0200

    'Refactor'

--- src/android/CardReader.java
+++ src/android/CardReader.java
@@ -11,14 +11,44 @@ import com.acs.bluetooth.BluetoothReaderGattCallback.OnConnectionStateChangeList
 
 import org.apache.cordova.CallbackContext;
 
-public class CardReader {
+public class CardReader extends CordovaPlugin {
+    private static final String CONNECT_READER = "connectReader";
+
     /* Bluetooth GATT client. */
     private BluetoothGatt mBluetoothGatt;
     private BluetoothReaderGattCallback mGattCallback;
 
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+
+        Runnable runnable = new Runnable() {
+            public void run() {
+                SumUpState.init(cordova.getActivity());
+            }
+        };
+
+        cordova.getActivity().runOnUiThread(runnable);
+    }
+
+
+    @Override
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+        if(action.equalsIgnoreCase(CONNECT_READER)){
+            connectReader(callbackContext, data)
+        } else {
+            return false;
+        }
+
+        return true;
+    }
+
+
     private String connectReader(final CallbackContext callbackContext, String[] data) {
-        String mDeviceAddress = data.toString();
-        return mDeviceAddress;
+        String myDeviceAddress = data.getString(0);
+        callbackContext.success(myDeviceAddress)
+
+
         // BLUETOOTH_SERVICE
         // BluetoothManager bluetoothManager = (BluetoothManager) this.getSystemService(Context.);
         // if (bluetoothManager == null) {
--- www/acs.js
+++ www/acs.js
@@ -1,11 +1,29 @@
+var exec = cordova.require('cordova/exec');
+
 // Empty constructor
-var ACS = {}
+var ACS = function () {
+    console.log('ACS instanced');
+};
+
+ACS.prototype.init = function (msg, onSuccess, onError) {
+    var errorCallback = function (obj) {
+        onError(obj);
+    };
+
+    var successCallback = function (obj) {
+        onSuccess(obj);
+    };
+
+    exec(successCallback, errorCallback, 'CardReader', 'init', [msg]);
+};
 
 // The function that passes work along to native shells
 // Message is a string, duration may be 'long' or 'short'
 
-ACS.connectReader = function (readerAddress, success, failure) {
+ACS.prototype.connectReader = function (readerAddress, success, failure) {
   cordova.exec(success, failure, "CardReader", "connectReader", [readerAddress]);
 };
 
-window.ACS = ACS;
+if (typeof module != 'undefined' && module.exports) {
+    module.exports = ACS;
+}
