806e7348cf9c484b7a2d98e430ef2ca60bf1fd66
louvkii <0Elfonfire@gmail.com>
2018-06-11 10:46:06 +0200

    'eh'

--- www/acs.d.ts
+++ www/acs.d.ts
@@ -5,15 +5,15 @@ declare module 'cordova-plugin-acs' {
 
     export function authenticate(): Promise<void>;
 
-    export function listenForAdpuResponse(): Observable<any>;
+    export function listenForAdpuResponse(resolve, reject): void;
 
-    export function listenForCardStatusAvailable(): Promise<void>;
+    export function listenForCardStatusAvailable(resolve, reject): void;
 
     export function startPolling(): Promise<void>;
 
     export function stopPolling(): Promise<void>;
 
-    export function startScan(): Observable<string>;
+    export function startScan(resolve, reject): void;
 
     export function stopScan(): Promise<void>;
 
